1a33d0d96f3f439f8e911aee381fdd3f933104c8
CarlosDuarte <ra52208844840@acaonsfatima.org.br>
2024-10-05 21:25:53 -0300

    Certin

--- src/controller/APIstreamingController.js
+++ src/controller/APIstreamingController.js
@@ -17,19 +17,126 @@ endpoint.post('/inserirCanal/', async (req, resp) => {
   }
 });
 
+endpoint.get('/consultarCanal/', async (req, resp) => {
+  try {
+    let registros = await db.consultarCanal();
+    resp.send(registros);
+  }
+  catch (err) {
+    resp.status(400).send({
+      erro: err.message
+    })
+  }
+});
+
+endpoint.put('/alterarCanal/:id', async (req, resp) => {
+  try {
+    let id = req.params.id;
+    let perfil = req.body;
+    let lineaffect = await db.alterarCanal(id, canal);
+    if (lineaffect >= 1) {
+      resp.send();
+    } 
+    else {
+      resp.status(404).send({ erro: 'Nenhum registro encontrado' })
+    }
+
+  }
+  catch (err) {
+    resp.status(400).send({
+      erro: err.message
+    })
+  }
+})
+
+
+
+endpoint.delete('/deletarCanal/:id', async (req, resp) => {
+  try {
+    let id = req.params.id;
+
+    let lineaffect = await db.removerCanal(id);
+    if (lineaffect >= 1) {
+      resp.send();
+    }
+    else {
+      resp.status(404).send({ erro: 'Nenhum registro encontrado' })
+    }
+
+  }
+  catch (err) {
+    resp.status(400).send({
+      erro: err.message
+    })
+  }
+})
+
 endpoint.post('/inserirPrograma/', async (req, resp) => {
   try {
     let programa = req.body;
     let id = await db.inserirPrograma(programa);
     resp.send({
       novoId: id
-    });
+    }); 
   } catch (err) {
     resp.status(400).send({
       erro: err.message
     });
   }
 });
+
+endpoint.get('/consultarPrograma/', async (req, resp) => {
+  try {
+    let registros = await db.consultarPrograma();
+    resp.send(registros);
+  }
+  catch (err) {
+    resp.status(400).send({
+      erro: err.message
+    })
+  }
+});
+
+endpoint.put('/alterarPrograma/:id', async (req, resp) => {
+  try {
+    let id = req.params.id;
+    let perfil = req.body;
+    let lineaffect = await db.alterarPrograma(id, programa);
+    if (lineaffect >= 1) {
+      resp.send();
+    } 
+    else {
+      resp.status(404).send({ erro: 'Nenhum registro encontrado' })
+    }
+
+  }
+  catch (err) {
+    resp.status(400).send({
+      erro: err.message
+    })
+  }
+})
+
+
+endpoint.delete('/deletarPrograma/:id', async (req, resp) => {
+  try {
+    let id = req.params.id;
+
+    let lineaffect = await db.removerPrograma(id);
+    if (lineaffect >= 1) {
+      resp.send();
+    }
+    else {
+      resp.status(404).send({ erro: 'Nenhum registro encontrado' })
+    }
+
+  }
+  catch (err) {
+    resp.status(400).send({
+      erro: err.message
+    })
+  }
+})
 
 endpoint.post('/inserirUsuario/', async (req, resp) => {
   try {
@@ -45,6 +152,61 @@ endpoint.post('/inserirUsuario/', async (req, resp) => {
   }
 });
 
+endpoint.get('/consultarUsuario/', async (req, resp) => {
+  try {
+    let registros = await db.consultarUsuario();
+    resp.send(registros);
+  }
+  catch (err) {
+    resp.status(400).send({
+      erro: err.message
+    })
+  }
+});
+
+endpoint.put('/alterarUsuario/:id', async (req, resp) => {
+  try {
+    let id = req.params.id;
+    let perfil = req.body;
+    let lineaffect = await db.alterarUsuario(id, usuario);
+    if (lineaffect >= 1) {
+      resp.send();
+    } 
+    else {
+      resp.status(404).send({ erro: 'Nenhum registro encontrado' })
+    }
+
+  }
+  catch (err) {
+    resp.status(400).send({
+      erro: err.message
+    })
+  }
+})
+
+
+endpoint.delete('/deletarUsuario/:id', async (req, resp) => {
+  try {
+    let id = req.params.id;
+
+    let lineaffect = await db.removerUsuario(id);
+    if (lineaffect >= 1) {
+      resp.send();
+    }
+    else {
+      resp.status(404).send({ erro: 'Nenhum registro encontrado' })
+    }
+
+  }
+  catch (err) {
+    resp.status(400).send({
+      erro: err.message
+    })
+  }
+})
+
+
+
 endpoint.post('/inserirProgramaFavorito/', async (req, resp) => {
   try {
     let favorito = req.body;
@@ -58,5 +220,59 @@ endpoint.post('/inserirProgramaFavorito/', async (req, resp) => {
     });
   }
 });
+
+endpoint.get('/consultarProgramaFavorito/', async (req, resp) => {
+  try {
+    let registros = await db.consultarProgramaFavorito();
+    resp.send(registros);
+  }
+  catch (err) {
+    resp.status(400).send({
+      erro: err.message
+    })
+  }
+});
+
+endpoint.put('/alterarProgramaFavorito/:id', async (req, resp) => {
+  try {
+    let id = req.params.id;
+    let perfil = req.body;
+    let lineaffect = await db.alterarProgramaFavorito(id, programafavorito);
+    if (lineaffect >= 1) {
+      resp.send();
+    } 
+    else {
+      resp.status(404).send({ erro: 'Nenhum registro encontrado' })
+    }
+
+  }
+  catch (err) {
+    resp.status(400).send({
+      erro: err.message
+    })
+  }
+})
+
+
+endpoint.delete('/deletarProgramaFavorito/:id', async (req, resp) => {
+  try {
+    let id = req.params.id;
+
+    let lineaffect = await db.removerProgramaFavorito(id);
+    if (lineaffect >= 1) {
+      resp.send();
+    }
+    else {
+      resp.status(404).send({ erro: 'Nenhum registro encontrado' })
+    }
+
+  }
+  catch (err) {
+    resp.status(400).send({
+      erro: err.message
+    })
+  }
+})
+
 
 export default endpoint
--- src/repository/APIstreamingRepository.js
+++ src/repository/APIstreamingRepository.js
@@ -3,9 +3,7 @@ import con from './connection.js';
 export async function inserirCanal(canal) {
   const comando = `
     INSERT INTO tb_canal (id_canal , nm_canal, nr_canal, bt_aberto )
-    VALUES (?, ?, ?, ? )
-
-
+    VALUES (?, ?, ?, ? )    
   `;
 
   let resposta = await con.query(comando, [
@@ -20,21 +18,62 @@ export async function inserirCanal(canal) {
   return info.insertId;
 }
 
+export async function consultarCanal() {
+  const comando = `
+    select id_canal           as id,
+          nm_canal            as nome,
+          nr_canal            as canal,
+          bt_aberto           as publico
+    from tb_canal;
+  `;
+  let resposta = await con.query(comando);
+  let registro = resposta[0];
+
+  return registro;
+}
+
+export async function alterarCanal(id, canal) {
+  const comando = `
+    update tb_canal
+    set nm_canal = ?,
+        nr_canal = ?,
+        bt_aberto = ?
+    where id_canal = ?;
+  `;
+  let resposta = await con.query(comando, [
+    canal.nome,
+    canal.canal,
+    canal.publico,
+    id,
+  ]);
+  let info = resposta[0];
+
+  return info.affectedRows;
+
+};
+
+export async function removerCanal(id) {
+  const comando = `
+    delete from tb_canal
+    where id_canal = ?;
+  `;
+  let resposta = await con.query(comando, [id]);
+  let info = resposta[0];
+
+  return info.affectedRows;
+}
+
+
 export async function inserirPrograma(programa) {
   const comando = `
     INSERT INTO tb_canal_programa (id_canal_programa , id_canal, nm_programa, ds_genero, hr_programa )
-    VALUES (?, ?, ?, ?, ? );  
-
-    select p.id_canal
-    from tb_canal 
-    inner join tb_canal_programa p
-    on tb_canal.id_canal = p.id_canal;
+    VALUES (?, ?, ?, ?, ?);
   `;
 
   let resposta = await con.query(comando, [
     programa.id,
     programa.id_canal,
-    programa.programa,
+    programa.nome,  
     programa.genero,
     programa.horario
   ]);
@@ -42,7 +81,62 @@ export async function inserirPrograma(programa) {
   let info = resposta[0];
 
   return info.insertId;
-}
+  };
+
+  export async function consultarPrograma() {
+    const comando = `
+      SELECT 
+        id_canal_programa   AS id,
+        id_canal            AS id_canal,
+        nm_programa         AS nome,
+        ds_genero           AS genero,
+        hr_programa         AS tempo
+      FROM tb_canal_programa;
+    `;
+  
+    let resposta = await con.query(comando);
+    let registros = resposta[0];
+  
+    return registros;
+  }
+
+  export async function alterarPrograma(id, programa) {
+    const comando = `
+      update tb_canal_programa
+      set id_canal = ?,
+          nm_programa = ?,
+          ds_genero = ?,
+          hr_programa = ?
+      where id_canal_programa = ?;
+    `;
+    let resposta = await con.query(comando, [
+      programa.id_canal,
+      programa.nome,
+      programa.genero,
+      programa.horario,
+      id,
+    ]);
+    let info = resposta[0];
+  
+    return info.affectedRows;
+  
+  };
+  
+
+  export async function removerPrograma(id) {
+    const comando = `
+      delete from tb_canal_programa
+      where id_canal_programa = ?;
+    `;
+    let resposta = await con.query(comando, [id]);
+    let info = resposta[0];
+  
+    return info.affectedRows;
+  }
+  
+  
+
+
 
 export async function inserirUsuario(usuario) {
   const comando = `
@@ -60,11 +154,52 @@ export async function inserirUsuario(usuario) {
   return info.insertId;
 }
 
+export async function consultarUsuario() {
+  const comando = `
+    select id_usuario           as id,
+           nm_usuario          as nome
+    from tb_usuario;
+  `;
+  let resposta = await con.query(comando);
+  let registro = resposta[0];
+
+  return registro;
+}
+
+export async function alterarUsuario(id, usuario) {
+  const comando = `
+    update tb_usuario
+      set nm_usuario = ?
+    where id_usuario = ?;
+  `;
+  let resposta = await con.query(comando, [
+    usuario.nome,
+    id,
+  ]);
+  let info = resposta[0];
+
+  return info.affectedRows;
+
+};
+
+
+export async function removerUsuario(id) {
+  const comando = `
+    delete from tb_usuario
+    where id_usuario = ?;
+  `;
+  let resposta = await con.query(comando, [id]);
+  let info = resposta[0];
+
+  return info.affectedRows;
+}
+
+
 export async function inserirProgramaFavorito(favorito) {
   const comando = `
     INSERT INTO tb_programa_favorito (id_programa_favorito , id_usuario, id_canal_programa )
-    VALUES (?, ?, ?)
-  `;
+    VALUES (?, ?, ?);
+    `;
 
   let resposta = await con.query(comando, [
     favorito.id,
@@ -75,4 +210,50 @@ export async function inserirProgramaFavorito(favorito) {
   let info = resposta[0];
 
   return info.insertId;
+  
+}
+
+export async function consultarProgramaFavorito() {
+  const comando = `
+    select id_programa_favorito    as id,
+           id_usuario              as id_usuario,
+           id_canal_programa       as id_programa
+    from tb_programa_favorito;
+
+  `;
+  let resposta = await con.query(comando);
+  let registro = resposta[0];
+
+  return registro;
+};
+
+export async function alterarProgramaFavorito(id, programafavorito) {
+  const comando = `
+    update tb_produto
+    set id_usuario = ?,
+        id_canal_programa = ?
+    where id_programa_favorito = ?;
+  `;
+  let resposta = await con.query(comando, [
+    programafavorito.id_usuario,
+    programafavorito.id_programa,
+    id,
+  ]);
+  let info = resposta[0];
+
+  return info.affectedRows;
+
+};
+
+
+
+export async function removerProgramaFavorito(id) {
+  const comando = `
+    delete from tb_programa_favorito
+    where id_programa_favorito = ?;
+  `;
+  let resposta = await con.query(comando, [id]);
+  let info = resposta[0];
+
+  return info.affectedRows;
 }
